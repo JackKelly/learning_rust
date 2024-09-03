33d5a8cc5a4fcaaf3e08f1960e1c037445a9eed5
Jack Kelly <jack@openclimatefix.org>
2024-09-03 19:08:01 +0100

    Simpler! Can use two runtimes, one inside the other

--- tokio_two_runtimes/src/main.rs
+++ tokio_two_runtimes/src/main.rs
@@ -3,28 +3,38 @@ use std::time::Duration;
 use tokio::runtime::Builder;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Start Tokio runtime on separate thread for CPU-bound tasks:
-    let cpu_thread_handle = std::thread::spawn(|| {
-        let cpu_rt = Builder::new_multi_thread()
-            .worker_threads(2)
-            .build()
-            .expect("Building Tokio runtime for CPU-bound tasks");
-        cpu_rt.block_on(async {
-            println!("Starting task on cpu_rt! {:?}", std::thread::current().id());
-            std::thread::sleep(Duration::from_secs(3));
-            println!("Ending task on cpu_rt! {:?}", std::thread::current().id());
-        })
-    });
+    let main_rt = Builder::new_multi_thread().worker_threads(2).build()?;
+    let cpu_rt = Builder::new_multi_thread().worker_threads(2).build()?;
 
-    // Start Tokio runtime for IO tasks on local thread:
-    let io_rt = Builder::new_multi_thread().worker_threads(2).build()?;
-    io_rt.block_on(async {
-        println!("Starting task on io_rt! {:?}", std::thread::current().id());
-        std::thread::sleep(Duration::from_secs(3));
-        println!("Ending task on io_rt! {:?}", std::thread::current().id());
-    });
+    // Adapted from https://stackoverflow.com/a/61763027
 
-    cpu_thread_handle.join().unwrap();
+    // Tokio reports an error if one runtime is dropped within another runtime. So we must drop
+    // the cpu runtime in the main thread, and only move a clone of the handle into the main_rt
+    let cpu_rt_handle = cpu_rt.handle().clone();
+
+    main_rt.block_on(async {
+        println!("Starting main_rt! {:?}", std::thread::current().id());
+
+        let _ = main_rt
+            .spawn(async move {
+                cpu_rt_handle
+                    .spawn(async {
+                        println!(
+                            "Starting task on cpu_rt_handle! {:?}",
+                            std::thread::current().id()
+                        );
+                        std::thread::sleep(Duration::from_secs(3));
+                        println!(
+                            "Ending task on cpu_rt_handle! {:?}",
+                            std::thread::current().id()
+                        );
+                    })
+                    .await
+            })
+            .await;
+
+        println!("Ending main_rt! {:?}", std::thread::current().id());
+    });
 
     Ok(())
 }
