56406d29649a8ea445397c09d805bcfd4ba17604
Jack Kelly <jack@openclimatefix.org>
2024-09-03 18:01:02 +0100

    Two Tokio runtimes work when using a separate thread for one runtime! Now to figure out how to pass Futures between the two runtimes!

--- tokio_two_runtimes/src/main.rs
+++ tokio_two_runtimes/src/main.rs
@@ -1,27 +1,30 @@
-use core::panic;
 use std::time::Duration;
 
 use tokio::runtime::Builder;
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cpu_rt = Builder::new_multi_thread().worker_threads(2).build()?;
-    let io_rt = Builder::new_multi_thread().worker_threads(2).build()?;
-
-    let cpu_handle = cpu_rt.spawn(async {
-        println!("Starting task on cpu_rt! {:?}", std::thread::current().id());
-        std::thread::sleep(Duration::from_secs(3));
-        println!("Ending task on cpu_rt! {:?}", std::thread::current().id());
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start Tokio runtime on separate thread for CPU-bound tasks:
+    let cpu_thread_handle = std::thread::spawn(|| {
+        let cpu_rt = Builder::new_multi_thread()
+            .worker_threads(2)
+            .build()
+            .expect("Building Tokio runtime for CPU-bound tasks");
+        cpu_rt.block_on(async {
+            println!("Starting task on cpu_rt! {:?}", std::thread::current().id());
+            std::thread::sleep(Duration::from_secs(3));
+            println!("Ending task on cpu_rt! {:?}", std::thread::current().id());
+        })
     });
 
-    let io_handle = io_rt.spawn(async {
+    // Start Tokio runtime for IO tasks on local thread:
+    let io_rt = Builder::new_multi_thread().worker_threads(2).build()?;
+    io_rt.block_on(async {
         println!("Starting task on io_rt! {:?}", std::thread::current().id());
         std::thread::sleep(Duration::from_secs(3));
         println!("Ending task on io_rt! {:?}", std::thread::current().id());
     });
 
-    cpu_handle.await?;
-    io_handle.await?;
+    cpu_thread_handle.join().unwrap();
 
     Ok(())
 }
